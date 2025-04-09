8daebed7932aee0011f2a5247ec8cd6ad29d0f13
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-04-09 12:54:58 +0200

    Update mod.rs

--- src/launcher/mod.rs
+++ src/launcher/mod.rs
@@ -1,10 +1,11 @@
 // In launcher/mod.rs
-pub mod config; // Change to pub to allow access from microsoft_auth_impl.rs
+pub mod config;
 mod process;
 
 // Correct imports
 pub use config::{update_jvm_args, get_jvm_args};
-pub use config::get_minecraft_dir; // Explicitly re-export this function
+pub use config::get_minecraft_dir;
+pub use process::launch_modpack;
 
 // Import log macros
 use log::{debug, warn};
