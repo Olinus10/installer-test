bdbf3fcff84f80dbdb6c8cddb3eac24d365da971
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-04-10 20:06:17 +0200

    Update mod.rs

--- src/launcher/mod.rs
+++ src/launcher/mod.rs
@@ -4,8 +4,7 @@ mod process;
 
 // Correct imports
 pub use config::{update_jvm_args, get_jvm_args};
-pub use config::get_minecraft_dir;
 pub use process::launch_modpack;
 
 // Import log macros
-use log::{debug, warn};
+use log::info;
