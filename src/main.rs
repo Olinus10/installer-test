f60f8a2a0421f47adf9b931442698dec656ba1c7
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-06-17 19:58:18 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ mod universal;
 pub use launcher::{launch_modpack, update_jvm_args, get_jvm_args};
 pub use installation::{Installation, get_active_installation, load_all_installations};
 pub use preset::{Preset, load_presets};
-pub use universal::{UniversalManifest, load_universal_manifest};
+pub use universal::{UniversalManifest, load_universal_manifest, ModComponent};
 pub use universal::{ManifestError, ManifestErrorType};
 
 // Remove authentication related code
