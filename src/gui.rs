0436a12b7eac74f38874f9c918a4a206bcc409ee
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-07-06 22:35:18 +0200

    Update gui.rs

--- src/gui.rs
+++ src/gui.rs
@@ -3073,15 +3073,17 @@ let feature_styles = include_str!("assets/expanded-feature-styles.css");
 let preset_styles = include_str!("assets/preset-styles.css");
 let search_styles = include_str!("assets/search-results-styles.css");
 let modal_styles = include_str!("assets/modal-styles.css");
+let installation_header_styles = include_str!("assets/installation-header-styles.css");
 
 // Combine all CSS files
-let complete_css = format!("{}\n{}\n{}\n{}\n{}\n{}", 
+let complete_css = format!("{}\n{}\n{}\n{}\n{}\n{}\n{}", 
     css_content, 
     category_styles, 
     feature_styles, 
     preset_styles, 
     search_styles,
-    modal_styles
+    modal_styles,
+    installation_header_styles
 );
 
     let mut modal_context = use_context_provider(ModalContext::default);
